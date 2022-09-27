2ba1b057bc0a8953584c765d2865a7b3e16e1de3
Lucas Santana Felippe <106406947+lucassantanafelippecomputerforinternet@users.noreply.github.com>
2022-09-27 10:36:32 -0300

    commit1

---
+++ recuperacao/a12.js
@@ -0,0 +1,5 @@
+document.getElementById("text1").style.color = "green";
+document.getElementById("text1").style.fontSize = "40px";
+document.getElementById("text2").style.color = "gray";
+document.getElementById("text2").style.fontSize = "30px";
+document.getElementsByClassName("text3")[0].innerHTML = "Conteúdo: DOM e JSON";
---
+++ recuperacao/page/page.js
@@ -0,0 +1,12 @@
+function Produto() {
+    document.getElementById("produto").src = "cblack.webp";
+}
+function mudarProduto() {
+    document.getElementById("produto").src = "cazul.webp";
+}
+function mudarProduto2() {
+    document.getElementById("produto").src = "perto.webp";
+}
+function mudarProduto3() {
+    document.getElementById("produto").src = "azull.webp";
+}
